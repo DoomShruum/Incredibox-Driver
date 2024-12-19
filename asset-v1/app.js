9aaedec184ebf43da877519f208a7c506060f995
DoomShruum <shruumstudios@gmail.com>
2024-12-19 17:57:04 -0500

    Update app.js
    Added sounds into database

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,37 +1,37 @@
 /* Finally, some good fucking json */
 var app = new function() {
     this.name = "Pushing Life", this.version = "A", this.date = "2022", this.folder = "asset-v1/", this.looptime = 8000, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 3, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 68, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#1b1851", this.col1 = "#1b1851", this.col2 = "#12103d", this.col3 = "#0d0b30", this.col4 = "#232323", this.animearray = [{
-        name: "1_Driver",
+        name: "1_Kick",
         color: "b816ad",
         uniqsnd: !0
     }, {
-        name: "2_boom",
+        name: "2_Snare",
         color: "b816ad",
         uniqsnd: !0
     }, {
-        name: "3_snap",
+        name: "3_Hats",
         color: "b816ad",
         uniqsnd: !0
     }, {
-        name: "4_phsht",
+        name: "4_Open",
         color: "b816ad",
         uniqsnd: !0
     }, {
-        name: "5_clap",
+        name: "5_Riser",
         color: "b816ad",
         uniqsnd: !0
     }, {
-        name: "6_bass",
+        name: "6_Bass",
         color: "3a5af0",
-        uniqsnd: !0
+        uniqsnd: !1
     }, {
-        name: "7_doom",
+        name: "11_Oh",
         color: "3a5af0",
-        uniqsnd: !0
+        uniqsnd: !1
     }, {
-        name: "8_aeugh",
+        name: "12_Do",
         color: "3a5af0",
-        uniqsnd: !0
+        uniqsnd: !1
     }], this.bonusarray = [];
     for (var n = 0, o = this.animearray.length; n < o; n++) {
 		var a = this.animearray[n].name;
